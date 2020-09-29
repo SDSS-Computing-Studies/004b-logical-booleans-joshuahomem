d9125a1c2aec1b62abd6c579ceb7f44301ca4832
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-09-29 12:16:39 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -15,3 +15,9 @@ example:
 Enter a number: -3
 -3 is not a positive integer
 """
+a=input("enter in any number")
+a=float(a)
+if a>0:
+    print(str(a)+" is a posotive integer")
+elif a<0:
+    print(str(a)+" is not a posotive integer")
