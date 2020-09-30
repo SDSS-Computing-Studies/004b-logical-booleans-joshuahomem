c226a154a011a54aa2672df84241e5ec741f266e
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-09-30 12:08:41 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -23,3 +23,20 @@ Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
+a=input("an integer ")
+b=input("an integer ")
+a=int(a)
+b=int(b)
+if a>b: 
+    number1=a%b
+    if number1==0:
+        print(str(b)+" is a factor of " +str(a))
+    else:
+        print(str(b)+" is not a factor of "+str(a))
+
+elif a<b:
+    number2=b%a
+    if number2==0:
+        print(str(a) +" is a factor of a "+str(b))
+    else:
+        print(str(a)+" is not a factor of a "+str(b))
