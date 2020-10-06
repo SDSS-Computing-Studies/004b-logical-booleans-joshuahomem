0e622fde6f47fb26fb71321aacfc1455c3bd7128
joshuahomem <71406869+joshuahomem@users.noreply.github.com>
2020-10-06 12:41:23 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -1,3 +1,4 @@
+  
 #! python3
 
 """
@@ -5,35 +6,33 @@ Problem 2
 Factors are positive integers that divide evenly into another integer.
 The user will enter in two numbers. Determine if the smaller is a factor of the larger
 (2 marks)
-
 inputs:
 an integer
 an integer
-
 outputs:
 xx is a factor of yy
 xx is not a factor of yy
-
 examples:
 Enter a number: 10
 Enter another number: 2
 2 is a factor of 10
-
 Enter a number: 4
 Enter another number: 25
 4 is not a factor of 25
 """
-a=input("an integer ").strip()
-b=input("an integer ").strip()
-a=int(a)
-b=int(b)
-if a>b: 
-    number1=a%b
-    if number1==0:
-        print(str(b)+" is a factor of " +str(a))
-    else:
-        print(str(b)+" is not a factor of "+str(a))
-
+number1=int(input("Enter an integer "))
+number2=int(input('Enter another integer '))
+if (number1>number2)==True:
+    gNum=number1
+    lNum=number2
+else:
+    gNum=number2
+    lNum=number1
+value=gNum%lNum
+if value==0:
+    print(str(lNum) + " is a factor of " + str(gNum))
+else: 
+    print(str(lNum) + " is not a factor of " + str(gNum))
 elif a<b:
     number2=b%a
     if number2==0:
